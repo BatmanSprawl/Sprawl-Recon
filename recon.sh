bec745a61a34e89b54bc99743da721c274e74c03
Mr-Sprawl <36556360+Mr-Sprawl@users.noreply.github.com>
2019-08-16 12:48:02 -0500

    Recon 2.0
    Make it prettier, add more function

--- recon.sh
+++ recon.sh
@@ -4,7 +4,9 @@ mkdir $machine
 cd $machine
 read -p "What is the IP: " ip
 echo $ip > ip.txt
+
 echo "###############     STARTING     ###############"
+
 echo " Machine : $machine"
 echo -n " IP : " && cat ip.txt
 echo -n " Ports : " 
@@ -12,18 +14,79 @@ nmap -sS -p- -iL ip.txt >> ports.txt
 cat ports.txt | grep tcp | cut -d / -f 1 | xargs | sed -e 's/ /,/g' >> pl.txt
 cat pl.txt
 echo -n " Version Scan"
-for port in $(cat pl.txt);do nmap -sS -sV -p $port -iL ip.txt >> version.txt;done
-echo " . . . . . . Done"
-echo -n " Aggressive Scan"
-nmap -A -iL ip.txt >> aggro.txt
-echo ". . . . . Done"
-echo -n " OS Scan"
-for port in $(cat pl.txt);do nmap -O -p $port -iL ip.txt >> os.txt;done
-echo ". . . . . . . . . Done"
-echo -n " Standard Scan"
-nmap -sC -iL ip.txt >> standscan.txt
-echo ". . . . . . Done"
-echo -n " Vulnerability Scan"
-for port in $(cat pl.txt);do nmap -p $port --script vuln -iL ip.txt >> vuln.txt;done
-echo " . . . Done"
+
+for port in $(cat pl.txt);do 
+	nmap -sS -sV -p $port -iL ip.txt >> version.txt
+	echo " . . . . . . Done"
+	echo -n " Aggressive Scan"
+	nmap -A -iL ip.txt >> aggro.txt
+	echo ". . . . . Done"
+	echo -n " OS Scan"
+	nmap -O -p $port -iL ip.txt >> os.txt
+	echo ". . . . . . . . . Done"
+	echo -n " Standard Scan"
+	nmap -sC -iL ip.txt >> standscan.txt
+	echo ". . . . . . Done"
+	echo -n " Vulnerability Scan"
+	nmap -p $port --script vuln -iL ip.txt >> vuln.txt
+	echo " . . . Done"
+;done
+
+for ps in $(cat ports.txt | grep http | cut -d / -f 1);do
+	if [ $ps != "" ]
+	then
+		echo "      .:Http Port Found:.      "
+		echo " Http Scans Starting"
+		echo -n " Nmap"
+		nmap --script http-enum -T 4 $ip >> http-enum-$ps.txt
+		echo " . . . . . . . . . . Done" 
+		echo -n " Nikto"
+		nikto -h http://$ip >> nikto-basic-$ps.txt
+		echo ". . . . . . . . . . Done"
+		echo -n " Dirb"
+		dirb http://$ip/ >> dirb-basic-$ps.txt
+		echo " . . . . . . . . . . Done"
+	elif [ $ps = "25" ] || [ $ps = "465" ] || [ $ps = "587" ] || [ $ps = "2525" ]
+	then
+		echo $ps >> smtptemp.txt
+	elif [ $ps = "139" ] || [ $ps = "445" ]
+	then
+		echo $ps >> smbtempt.txt
+	fi
+;done
+
+if [ -e smtptemp.txt ]
+then
+	cat smtptemp.txt | xargs | sed -e 's/ /,/g' >> smtpports.txt
+	for smp in $(cat smtpports.txt);do
+		echo "      .:SMTP Ports Found:.     "
+		echo " SMTP Scans Starting"
+		echo -n " Nmap"
+		nmap --script smtp-enum-users -p $smtp $ip >> smtp-enum-users.txt
+		nmap --script smbtp-commands -p $smtp $ip >> smtp-commands.txt
+		echo " . . . . . . . . . . Done"
+	;done
+	rm smtptemp.txt
+fi
+
+if [ -e smbtempt.txt]
+then
+	cat smbtemp.txt | xargs | sed -e 's/ /,/g' >> smbports.txt
+	for smb in $(cat smbports.txt);do
+		echo "      .:SMB Ports Found:.     "
+		echo " SMB Scans Starting"
+		echo -n " Nmap"
+		nmap --script smb-enum-users -p $smb $ip >> smb-users.txt
+		nmap --script smb-os-discovery -p $smb $ip >> smb-os.txt
+		nmap --script smb-security-mode -p $smb $ip >> smb-security.txt
+		echo " . . . . . . . . . . Done"
+		echo -n " Enum4linux"
+		enum4linux $ip >> enum4linux.txt
+		echo " . . . . . . . Done"
+	;done
+	rm smbtempt.txt
+fi
+
+rm pl.txt
+
 echo "###############     COMPLETE     ###############"
